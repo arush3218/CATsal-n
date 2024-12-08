29c5eeb93306f8a565c664feb4df49593fcb521a
arush3218 <arush3218@gmail.com>
2024-12-08 23:41:45 +0530

    big update completing backend

---
+++ catsaloon/base/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('login/', views.loginPage, name = 'login'),
+    path('register/', views.registerPage, name = 'register'),
+    path('logout/', views.logoutUser, name = 'logout'), 
+    path('', views.home, name = 'home'),
+    path('room/<str:pk>/', views.room, name = 'room'),
+    path("create-room.html/", views.createRoom, name = 'create_room'),
+    path('update-room/<str:pk>', views.updateRoom, name = 'update_room'),
+    path('delete-room/<str:pk>', views.DeleteRoom, name = 'delete_room'),
+    path('delete-message/<str:pk>', views.DeleteMessage, name = 'delete_message'),
+    path('profile/<str:pk>/', views.userProfile, name="user_profile"),
+    
+]
